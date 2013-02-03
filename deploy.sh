a3738a5090e05c99b04e16934bc0dd82d9b5305d
Andrew Montalenti <andrew@parsely.com>
2013-02-03 10:57:39 -0500

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -avz --progress ./ cogtree@pixelmonkey.org:/data/vhosts/pixelmonkey.org/pub/python-crawling-slides
+rsync -avz --progress ./ cogtree@pixelmonkey.org:/data/vhosts/pixelmonkey.org/pub/rapid-web-slides
